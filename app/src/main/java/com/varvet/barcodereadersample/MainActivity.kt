1dc3569aab3303fc16db07ae71fc744f5c2cc56f
Daniell Algar <daniell.algar@gmail.com>
2018-01-17 10:35:55 +0100

    Convert MainActivity to Kotlin

---
+++ app/src/main/java/com/varvet/barcodereadersample/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.varvet.barcodereadersample
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.vision.barcode.Barcode
+import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mResultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mResultTextView = findViewById(R.id.result_textview)
+
+        findViewById<Button>(R.id.scan_barcode_button).setOnClickListener {
+            val intent = Intent(applicationContext, BarcodeCaptureActivity::class.java)
+            startActivityForResult(intent, BARCODE_READER_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == BARCODE_READER_REQUEST_CODE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
+                    val p = barcode.cornerPoints
+                    mResultTextView.text = barcode.displayValue
+                } else
+                    mResultTextView.setText(R.string.no_barcode_captured)
+            } else
+                Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
+                        CommonStatusCodes.getStatusCodeString(resultCode)))
+        } else
+            super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.simpleName
+        private val BARCODE_READER_REQUEST_CODE = 1
+    }
+}
